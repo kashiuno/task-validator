c08f946f5ebb065e05ee01dda6100d7516e5d620
dakrupin <dakrupin@bank-hlynov.ru>
2020-11-19 14:39:59 +0300

    Features: Add strict mode for StringValidator.php

--- src/Validators/StringValidator.php
+++ src/Validators/StringValidator.php
@@ -2,14 +2,25 @@
 
 namespace Validator\Validators;
 
+use Validator\Exceptions\ValidateConfigurationException;
+
 class StringValidator extends AbstractValidator
 {
     protected string $message = 'Is not a string';
     public function __invoke($value): string
     {
-        if (!is_string($value)) {
+        if ($this->isStrict() && !is_string($value)) {
             return $this->message;
         }
         return '';
+    }
+
+    protected function validateConfig()
+    {
+        if (isset($this->parameters['strict']) && !is_bool($this->parameters['strict'])) throw new ValidateConfigurationException('Parameter strict must be a boolean');
+    }
+
+    private function isStrict() {
+        return isset($this->parameters['strict']) && $this->parameters['strict'];
     }
 }
