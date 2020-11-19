746949a6d34db54ec9387e461999dc90281e6f10
kashiuno <taigusiobaka@gmail.com>
2020-11-19 22:16:58 +0300

    Tests: Add new validators to stub

--- tests/Stubs/Forms/FormWithRules.php
+++ tests/Stubs/Forms/FormWithRules.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Stubs\Forms;
 
+use Validator\Validators\CallableValidator;
 use Validator\Validators\RegExpValidator;
 use Validator\Validators\RequireValidator;
 
@@ -12,6 +13,8 @@ class FormWithRules extends FormWithoutRules
         return [
             ['name' => new RegExpValidator('Specified string is not match the pattern', ['expression' => '/\\d/', 'match' => true])],
             ['name' => new RequireValidator('Name must be specify')],
+            ['name' => new CallableValidator('Name not assert to callable', ['callback' => function ($value) { return is_string($value); }])],
+            ['age' => new RequireValidator('Age must be specify')]
         ];
     }
 }
